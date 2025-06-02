7de392dda3e5dcf7ff29a1b4e6d42e4d38dd1f96
vaujan <vaujan@github.com>
2025-06-02 19:25:22 +0700

    Update ESLint configuration, add new dependencies, and enhance global styles. Removed unused SVG files and refactored the main page component to include a counter feature.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import { off } from "process";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,7 +11,9 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends("next/core-web-vitals", "next/typescript"), {rules: { 
+    "react/react-in-jsx-scope": off
+  }}
 ];
 
 export default eslintConfig;
